0f363934e0537eeda08215e3aa105e5b89827b46
Brady Snuggs <dronebug360@gmail.com>
2020-05-05 17:53:20 -0600

    configured fullCalendar without additional scss and webpack modifications.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Calendar from './views/calendar.js'
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Calendar />
     </div>
   );
 }
---
+++ src/views/calendar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import FullCalendar from '@fullcalendar/react';
+import resourceTimeGridPlugin from '@fullcalendar/resource-timegrid';
+
+const Calendar = () => {
+
+    return(
+        <FullCalendar 
+            defaultView="resourceTimeGrid"
+            plugins={[resourceTimeGridPlugin]}
+            
+            // events={[
+            // { title: 'event 1', date: '2020-05-05', time: '2am' },
+            // { title: 'event 2', date: '2020-05-05', time: '12pm' }
+            // ]}
+            resources={[{id: 'a', title: 'Doctor'}, {id: 'b', title: 'Assistant'}, {id: 'c', title: 'Hygienist'}]}
+            
+            datesAboveResources={true}
+            schedulerLicenseKey={'GPL-My-Project-Is-Open-Source'}
+        />
+    )
+}
+
+export default Calendar
