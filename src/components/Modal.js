2ccec7daf152d2f7167f2d11606256c89c2f73c5
Brady Snuggs <dronebug360@gmail.com>
2020-05-06 16:33:07 -0600

    finished modal state. Need to pass on to dispatch and then set db into redux.

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -5,20 +5,43 @@ import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import TimePicker from 'react-time-picker';
 
 const ModalPopup = (props) => {
   const {show, toggleModal} = props;
-  const [form, setForm] = useState({column: 'Choose...', date: new Date(), startTime: '', endTime: ''})
+  const [form, setForm] = useState({column: 'Choose...', date: new Date()})
+  const [startTime, setStartTime] = useState(new Date(new Date().setHours(0,0,0,0)))
+  const [endTime, setEndTime] = useState(new Date(new Date().setHours(12,0,0,0)))
 
-  
+  function createColumnOptions(){
+    const options = []
+
+    return(
+      {options}
+    )
+  }
+
   function submitForm(){
-    console.log('saving form data')
+    console.log(form, startTime, endTime, 'state to be submitted.')
+    if(startTime > endTime){
+      return alert('Warning: startTime is less than endTime')
+    }
     toggleModal()
+  }
+
+  function handleStartTime(e){
+    setStartTime(e)
+  }
+
+  function handleEndTime(e){
+    setEndTime(e)
   }
 
   function handleChange(e){
     if(e instanceof Date){
-      setForm({...form, date:e})
+      setForm({...form, date: e})
+    } else {
+      setForm({...form, column: e.target.value})
     }
   }
 
@@ -37,11 +60,14 @@ const ModalPopup = (props) => {
           <Form>
             <Form.Group as={Col} controlId="formGridColumn">
               <Form.Label>Column:</Form.Label>
-              <Form.Control as="select" value={form.column}>
-                <option>Choose...</option>
-                <option>Doctor</option>
-                <option>Assistant</option>
-                <option>Hygienist</option>
+              <Form.Control 
+                as="select" 
+                value={form.column}
+                onChange={handleChange}
+              >
+                <option key="1" value="Doctor">Doctor</option>
+                <option key="2" value="Assistant">Assistant</option>
+                <option key="3" value="Hygienist">Hygienist</option>
               </Form.Control>
             </Form.Group>
 
@@ -55,10 +81,22 @@ const ModalPopup = (props) => {
 
             <Form.Group as={Col} controlId="formGridStartTime">
               <Form.Label>Start:</Form.Label>
+              <TimePicker
+               name='startTime'
+               onChange={handleStartTime}
+               value={startTime}
+               disableClock={true}
+              />
             </Form.Group>
 
             <Form.Group as={Col} controlId="formGridEndTime">
               <Form.Label>End:</Form.Label>
+              <TimePicker
+               name='endTime'
+               onChange={handleEndTime}
+               value={endTime}
+               disableClock={true}
+              />
             </Form.Group>
             
           </Form>
