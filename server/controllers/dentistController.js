10935bf7d58f787f5f41e9324709dc1a7a40dc3c
Brady Snuggs <dronebug360@gmail.com>
2020-05-06 16:42:38 -0600

    adding simulated db json in dentistController.

--- server/controllers/dentistController.js
+++ server/controllers/dentistController.js
@@ -1,6 +1,21 @@
-const demoDB = [
+const demoDataBaseStructure = {
+    "clients": {
+        "Highland Family Practice": {
+            "owner": "Dr. Hopper",
+            "assistants": {
+                
+            },
+            "dentists": {
+                
+            },
+            "hygientists:": {
 
-]
+            },
+            "address": "4460 Highland Dr, Ste 400, Salt Lake City, UT 84124",
+            "phone": "+1 801 272 4111"
+        }
+    }
+}
 
 module.exports={
     getData: (req,res) => {
