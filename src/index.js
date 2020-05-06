a1ff30e5f38d799696c1336aca79131e17f15f66
Brady Snuggs <dronebug360@gmail.com>
2020-05-06 17:40:06 -0600

    hooking up store and provider with react app. Total overkill for what this demo is suppose to demonstrate, but would be important for a much larger application with many moving parts.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//Redux is total overkill for this demo, but I'd like to demo async actions with a redux store for a much larger application.
+const store = createStore(applyMiddleware(thunk))
 
+const render = () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>,
+    document.getElementById('root')
+  )
+}
+
+render()
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
---
+++ src/reducers/calendarReducer.js
@@ -0,0 +1,6 @@
+
+const calendarState = {
+
+}
+
+const calendar = 
